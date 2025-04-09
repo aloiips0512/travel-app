b3185292b1cbf1a7583af504a42da43512235a09
aloiips0512 <jaksic.draganaa@gmail.com>
2025-04-10 00:27:15 +0200

    initial server config and env implemented

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,40 @@
-import express from "express";
+import express, { Request, Response, Application } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import { supabase } from "./supabaseClient";
 
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT || 3000;
+
+const app: express.Application = express();
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, TS + express");
+  res.send("API is working!");
+});
+app.post("/trips", async (req: Request, res: Response): Promise<void> => {
+  const { user_id, name, description, start_date, end_date } = req.body;
+
+  if (!user_id || !name) {
+    res.status(400).json({ error: "user_id and name are required" });
+    return;
+  }
+
+  const { data, error } = await supabase
+    .from("trips")
+    .insert([{ user_id, name, description, start_date, end_date }])
+    .select()
+    .single();
+
+  if (error) {
+    res.status(500).json({ error: error.message });
+    return;
+  }
+  res.status(201).json(data);
+  return;
 });
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
